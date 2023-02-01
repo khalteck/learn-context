01f3e3337257a0f068d321d384d4c1e56593533a
khalteck <khalidoyeneye@gmail.com>
2023-02-01 17:40:26 +0100

    context learnt

--- src/components/Booklist.jsx
+++ src/components/Booklist.jsx
@@ -1,13 +1,18 @@
 import { useGlobalContext } from "../contexts/ThemeContext";
 
 const Booklist = () => {
-  const { handleTheme } = useGlobalContext();
+  const { handleTheme, isLightTheme, light, dark } = useGlobalContext();
+  const theme = isLightTheme ? light : dark;
+
   return (
-    <div className="book-list">
+    <div
+      className="book-list"
+      style={{ background: theme.ui, color: theme.syntax }}
+    >
       <ul>
-        <li>The way of water</li>
-        <li>Final empire</li>
-        <li>A song of ice and fire</li>
+        <li style={{ background: theme.ui }}>The way of water</li>
+        <li style={{ background: theme.ui }}>Final empire</li>
+        <li style={{ background: theme.ui }}>A song of ice and fire</li>
       </ul>
       <button className="btn" onClick={handleTheme}>
         Change theme
